f4558cf76942fd7e154c63cb060256339f506df3
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-10-25 10:29:03 -0300

    no se si hice algo

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -6,14 +6,17 @@ import { CursosComponent } from './cursos/cursos.component';
 import {CardModule} from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import {ImageModule} from 'primeng/image';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToastModule } from 'primeng/toast';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { LoginComponent } from './login/login.component';
 import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
 import { VerificarCorreoComponent } from './verificar-correo/verificar-correo.component';
 import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { Teoria1Component } from './teoria1/teoria1.component';
 import { Teoria2Component } from './teoria2/teoria2.component';
@@ -47,6 +50,8 @@ import { Teoria2Component } from './teoria2/teoria2.component';
     ToastrModule.forRoot(),
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
+    SplitButtonModule,
+    ToastModule
   ]
 })
 export class ComponentesModule { }
--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api'; 
+import {MenuItem} from 'primeng/api';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
+  providers: [MessageService],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {
+  items!: MenuItem[];
+  constructor(private primengConfig: PrimeNGConfig, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
-    
+    this.primengConfig.ripple = true;
+    this.items = [
+      {label: 'Cursos', icon: 'pi pi-refresh', command: () => {
+          this.update();
+      }},
+      {label: 'Delete', icon: 'pi pi-times', command: () => {
+          this.delete();
+      }},
+      {label: 'Cerrar sesión', icon: 'pi pi-info', url: 'http://angular.io'},
+      {separator:true},
+      {label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup']}
+  ];
   }
+  save(severity: string) {
+    this.messageService.add({severity: severity, summary:'Success', detail:'Data Saved'});
+}
 
+update() {
+    this.messageService.add({severity:'success', summary:'Success', detail:'Data Updated'});
+}
+
+delete() {
+    this.messageService.add({severity:'success', summary:'Success', detail:'Data Deleted'});
+}
 }
